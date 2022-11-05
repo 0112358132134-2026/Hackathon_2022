449c0204b572b74aad569cee6eddbf68efb5b20f
0112358132134-2026 <68541@cag.edu.gt>
2022-11-05 16:12:40 -0600

    Intento 1
    - Problema 1 OK
    - Problema 4 OK

---
+++ Solución/Solución/Funciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Solución
+{
+    class Funciones
+    {
+        public bool Scramble(string cadena_1, string cadena_2) {
+
+            if (cadena_1.Length != cadena_2.Length) {
+                return false;
+            }
+
+            for (int contador = 0; contador < cadena_1.Length; contador++)
+            {
+                if (cadena_2[0].Equals(cadena_1[contador]))
+                {
+                    cadena_1 = cadena_1.Remove(contador,1);
+                    cadena_2 = cadena_2.Remove(0,1);
+                    contador = -1;
+                }
+                if ((contador == cadena_1.Length - 1) && (cadena_1.Length != 0 && cadena_2.Length != 0)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int AbuelaBinaria(int N, int numero_1, int numero_2) {
+            if (numero_1 + numero_2 != N) {
+                return -1;
+            }
+            string binario_1 = Convert.ToString(numero_1, 2);
+            string binario_2 = Convert.ToString(numero_2,2);
+            string binarios =  binario_1 + binario_2;
+            string comparar = "10";
+            int cantidadUnos = 0;
+            for (int i = 0; i < binarios.Length; i++) {
+                if (binarios[i].Equals(comparar[0]))
+                {
+                    cantidadUnos += 1;
+                }
+            }            
+            return cantidadUnos;
+        }
+    }
+}
--- Solución/Solución/Program.cs
+++ Solución/Solución/Program.cs
@@ -6,15 +6,76 @@ namespace Solución
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("PROBLEMA 1");
-            Console.WriteLine("Ingresa la cadena No. 1");
-            string cadena_1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Ingresa la cadena No. 2");
-            string cadena_2 = Console.ReadLine().ToLower();            
+            Funciones _funciones = new();
 
-            Console.WriteLine(cadena_1 + cadena_2);
-            Console.ReadKey();
-
+            bool salir = false;            
+            while (!salir) {
+                bool opcionVaida = false;
+                while (!opcionVaida) {
+                    Console.WriteLine("MENÚ");
+                    Console.WriteLine("-----------------------------------------------");
+                    Console.WriteLine("1: Scramble");
+                    Console.WriteLine("2:");
+                    Console.WriteLine("3:");
+                    Console.WriteLine("4: Abuela Binaria");
+                    Console.WriteLine("5: Salir");
+                    Console.WriteLine("-----------------------------------------------");
+                    Console.WriteLine("Ingrese el número del problema que desea ver...");
+                    string texto = Console.ReadLine();
+                    int opcion = 0;
+                    try
+                    {
+                        opcion = Int32.Parse(texto);
+                        if (opcion >= 1 && opcion <= 5) {
+                            opcionVaida = true;
+                            Console.Clear();
+                            switch (opcion) {
+                                case 1:
+                                    Console.WriteLine("Ingrese la cadena 1: ");
+                                    string cadena1 = Console.ReadLine().ToLower();
+                                    Console.WriteLine("Ingrese la cadena 2: ");
+                                    string cadena2 = Console.ReadLine().ToLower();
+                                    if (_funciones.Scramble(cadena1, cadena2)) {
+                                        Console.WriteLine("¡La cadade 2 es una subcadena de la cadena 1!");
+                                    }
+                                    else {
+                                        Console.WriteLine("La cadade 2 no es una subcadena de la cadena 1");
+                                    }                                    
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Ingrese el numero total:");
+                                    int total = int.Parse(Console.ReadLine());
+                                    Console.WriteLine("Ingrese el numero uno:");
+                                    int numero_1 = int.Parse(Console.ReadLine());
+                                    Console.WriteLine("Ingrese el numero dos:");
+                                    int numero_2 = int.Parse(Console.ReadLine());
+                                    int res = _funciones.AbuelaBinaria(total, numero_1, numero_2);
+                                    if (res == -1) {
+                                        Console.WriteLine("Error: La suma de los 2 números no dan el total ingresado...");
+                                    }
+                                    else {
+                                        Console.WriteLine("La cantidad de avellanas es de: " + res.ToString());
+                                    }
+                                    break;
+                                case 5:
+                                    salir = true;
+                                    break;
+                            }
+                            Console.ReadKey();
+                        }
+                        Console.Clear();
+                    }
+                    catch (Exception)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Por favor, ingresa una opción válida...");
+                    }
+                }                
+            }                        
         }
     }
 }
