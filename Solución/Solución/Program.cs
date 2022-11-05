b9d618339c763a2d050f4c28309d0b66df391aa6
0112358132134-2026 <68541@cag.edu.gt>
2022-11-05 14:27:32 -0600

    Inicialización
    ...

---
+++ Solución/Solución/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solución
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROBLEMA 1");
+            Console.WriteLine("Ingresa la cadena No. 1");
+            string cadena_1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Ingresa la cadena No. 2");
+            string cadena_2 = Console.ReadLine().ToLower();            
+
+            Console.WriteLine(cadena_1 + cadena_2);
+            Console.ReadKey();
+
+        }
+    }
+}
